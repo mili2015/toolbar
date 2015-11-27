60155b27fac9b192855b60b52b84b03f38a03b53
mili2015 <mili.desenvolvimento@gmail.com>
2015-11-27 11:50:02 -0200

    Finish ApplicationTest
    Finish ApplicationTest

--- app/src/androidTest/java/br/com/mili/toolbar/ApplicationTest.java
+++ app/src/androidTest/java/br/com/mili/toolbar/ApplicationTest.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.test.ApplicationTestCase;
 
 /**
+ * This is a comment from github to test the checkout in AndroidStudio.
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
